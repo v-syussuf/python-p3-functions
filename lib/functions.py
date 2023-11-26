bfd6f993f7ecc69152aee136809f35efd8868576
Yussuf <yussuf.saumu@gmail.com>
2023-11-26 06:59:10 +0300

    Assignment Complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Naureen"):
+    print(f"Hello, ${name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+   print(f"Hello, ${name}!")
+
+greet_with_default("Sunny")   
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    return result
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
